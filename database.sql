2759ae73826f57b315c5d47d888a6ee6830ca4cc
Steve Burket <burketsteve@gmail.com>
2021-06-07 17:02:14 -0500

    hacking some css just to get familiar with lay of the land

--- database.sql
+++ database.sql
@@ -3,8 +3,91 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial NOT NULL,
+	"first_name" varchar(60) NOT NULL,
+	"last_name" varchar(60) NOT NULL,
+	"email" TEXT(50) NOT NULL UNIQUE,
+	"phone_number" integer(11) NOT NULL,
+	"username" varchar(80) NOT NULL UNIQUE,
+	"password" varchar(255) NOT NULL,
+	"is_instructor" BOOLEAN NOT NULL,
+	"instructor_is" integer NOT NULL,
+	"profile_pic" varchar(255),
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "lesson_plan" (
+	"id" serial NOT NULL,
+	"instrument_id" integer NOT NULL,
+	"teacher_id" integer NOT NULL,
+	"student_id" integer NOT NULL,
+	"lesson_time" TIMESTAMP NOT NULL,
+	"location_at" varchar(30) NOT NULL,
+	"lesson_notes" TEXT NOT NULL,
+	"assignment" varchar(255),
+	"scheduled_at" TIMESTAMP(10) NOT NULL,
+	"due_at" TIMESTAMP NOT NULL,
+	"submitted_url" TEXT NOT NULL,
+	"submitted_at" TIMESTAMP NOT NULL,
+	"submitted_comments" TEXT NOT NULL,
+	CONSTRAINT "lesson_plan_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "instruments" (
+	"id" serial NOT NULL,
+	"instrument" varchar(30) NOT NULL,
+	CONSTRAINT "instruments_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "practice_log" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"date_of" TIMESTAMP NOT NULL,
+	"practice_length" integer,
+	"topic" TEXT NOT NULL,
+	"improved_on" TEXT NOT NULL,
+	"weak_points" TEXT NOT NULL,
+	"questions" varchar(255) NOT NULL,
+	CONSTRAINT "practice_log_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "user_intstrument" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	CONSTRAINT "user_intstrument_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "user" ADD CONSTRAINT "user_fk0" FOREIGN KEY ("instructor_is") REFERENCES "user"("id");
+
+ALTER TABLE "lesson_plan" ADD CONSTRAINT "lesson_plan_fk0" FOREIGN KEY ("instrument_id") REFERENCES "instruments"("id");
+ALTER TABLE "lesson_plan" ADD CONSTRAINT "lesson_plan_fk1" FOREIGN KEY ("teacher_id") REFERENCES "user"("id");
+ALTER TABLE "lesson_plan" ADD CONSTRAINT "lesson_plan_fk2" FOREIGN KEY ("student_id") REFERENCES "user"("id");
+
+ALTER TABLE "instruments" ADD CONSTRAINT "instruments_fk0" FOREIGN KEY ("instrument") REFERENCES "user_intstrument"("id");
+
+ALTER TABLE "practice_log" ADD CONSTRAINT "practice_log_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
+ALTER TABLE "user_intstrument" ADD CONSTRAINT "user_intstrument_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function RegisterForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector((store) => store.errors);
@@ -13,6 +15,8 @@ function RegisterForm() {
     dispatch({
       type: 'REGISTER',
       payload: {
+        firstName: firstName,
+        lastName: lastName,
         username: username,
         password: password,
       },
@@ -20,19 +24,79 @@ function RegisterForm() {
   }; // end registerUser
 
   return (
-    <form className="formPanel" onSubmit={registerUser}>
+    <form className='formPanel' onSubmit={registerUser}>
       <h2>Register User</h2>
       {errors.registrationMessage && (
-        <h3 className="alert" role="alert">
+        <h3 className='alert' role='alert'>
           {errors.registrationMessage}
         </h3>
       )}
       <div>
-        <label htmlFor="username">
-          Username:
+        <label htmlFor='firstname'>
           <input
-            type="text"
-            name="username"
+            placeholder='First name'
+            type='text'
+            name='firstname'
+            value={firstName}
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor='lastname'>
+          <input
+            placeholder='Last name'
+            type='text'
+            name='lastname'
+            value={lastName}
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+      {/* </div>
+      <div>
+        <label htmlFor='username'>
+          <input
+            placeholder='email'
+            type='text'
+            name='username'
+            value={username}
+            required
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+      {/* </div>
+      <div>
+        <label htmlFor='username'>
+          <input
+            placeholder='Phone #'
+            type='text'
+            name='username'
+            value={username}
+            required
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+      </div>
+      <div> */} 
+        {/* <div>
+          <label htmlFor='username'>
+            <input
+            placeholder='Instrument'
+              type='text'
+              name='username'
+              value={username}
+              required
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </label>
+        </div> */}
+        <label htmlFor='username'>
+          <input
+          placeholder='Username'
+            type='text'
+            name='username'
             value={username}
             required
             onChange={(event) => setUsername(event.target.value)}
@@ -40,11 +104,11 @@ function RegisterForm() {
         </label>
       </div>
       <div>
-        <label htmlFor="password">
-          Password:
+        <label htmlFor='password'>
           <input
-            type="password"
-            name="password"
+          placeholder='Password'
+            type='password'
+            name='password'
             value={password}
             required
             onChange={(event) => setPassword(event.target.value)}
@@ -52,7 +116,7 @@ function RegisterForm() {
         </label>
       </div>
       <div>
-        <input className="btn" type="submit" name="submit" value="Register" />
+        <input className='btn' type='submit' name='submit' value='Register' />
       </div>
     </form>
   );
