a36f47bb0a13c1dbcbe2b85c68a545aa3cfe8b0a
Steve Burket <burketsteve@gmail.com>
2021-06-08 20:48:12 -0500

    practice log POST endpoint set

--- server/routes/practice_log.router.js
+++ server/routes/practice_log.router.js
@@ -12,9 +12,24 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/practice-log', (req, res) => {
   // POST route code here
-  
+  const user_id = req.body.user_id;
+  const date_of = req.body.date_of;
+  const practice_length = req.body.practice_length;
+  const topic = req.body.topic;
+  const improved_on = req.body.improved_on;
+  const weak_points = req.body.weak_points;
+  const questions = req.body.questions
+
+  const queryPracticeLog = `INSERT INTO "practice_log" ("user_id", "date_of", "practice_length", "topic", "improved_on", "weak_points", "questions")
+  VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING "user_id";`;
+  pool.query(queryPracticeLog, [user_id, date_of, practice_length, topic, improved_on, weak_points, questions])
+  .then(() => res.sendStatus(201))
+  .catch((err) => {
+      console.log('Error in practice log POST', err);
+      
+  })
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/practice_log', practiceLogRouter);
+app.use('/student', practiceLogRouter);
 
 // Serve static files
 app.use(express.static('build'));
