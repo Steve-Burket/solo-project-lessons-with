abff13221bcb6d72482dbb7210a641684e9d0b0b
Steve Burket <burketsteve@gmail.com>
2021-06-08 19:38:00 -0500

    added practice log route and added it to server

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const practiceLogRouter = require('./routes/practice_log.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/practice_log', practiceLogRouter);
 
 // Serve static files
 app.use(express.static('build'));
