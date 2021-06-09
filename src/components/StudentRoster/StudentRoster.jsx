adabe877c6b07a7a97982b7ffcd68afe1d1113e7
Steve Burket <burketsteve@gmail.com>
2021-06-08 19:14:57 -0500

    add student Form conditionally renders now

--- src/components/StudentRoster/StudentRoster.jsx
+++ src/components/StudentRoster/StudentRoster.jsx
@@ -10,22 +10,35 @@ function StudentRoster(props) {
   // a default value of 'Functional Component'
   const users = useSelector((store) => store.users);
   const [heading, setHeading] = useState('Student Roster');
-  const [addStudent, setAddStudent] = useState('');
+  const [addStudent, setAddStudent] = useState(false);
+  const [addFirstName, setAddFirstName] = useState('');
+  const [addLastName, setAddLasttName] = useState('');
+  const [addInstrument, setAddInstrument] = useState('');
+  const [viewAddForm, setAddForm] = useState(false);
 
   const studentList = ['Johnny Smith', 'Owen Goodman', 'John Johnson'];
   const studentInstrument = ['Guitar'];
 
   // ADD A STUDENT
-  const addStudentForm = (e) => {
-
-  }
+  const displayAddStudentForm = (e) => {
+    console.log('in displayAddStudentForm');
+    setAddForm(!viewAddForm);
+  };
 
   return (
     <div>
       <h2>{heading}</h2>
-      <button onClick={addStudentForm}>+ Add Student</button>
+      <button onClick={displayAddStudentForm}>+ Add Student</button>
       {/* Will eventually move Form to it's own Component*/}
-      <form></form>
+      <div>
+        {viewAddForm && (
+          <form>
+            <input placeholder='First name' />
+            <input placeholder='Last name' />
+            <input placeholder='Instrument' />
+          </form>
+        )}
+      </div>
       <table>
         <thead>
           <tr>
@@ -35,19 +48,19 @@ function StudentRoster(props) {
           </tr>
         </thead>
         <tbody>
-            {studentList.map((student, i, k) => {
-              return (
-                <tr>
-                  <td key={i}>{student}</td>
-                  {studentInstrument.map((instrument, j) => {
-                    return <td key={j}>{instrument}</td>;
-                  })}
-                  <td>
-                    <button key={k}>View</button>
-                  </td>
-                </tr>
-              );
-            })}
+          {studentList.map((student, i, k) => {
+            return (
+              <tr>
+                <td key={i}>{student}</td>
+                {studentInstrument.map((instrument, j) => {
+                  return <td key={j}>{instrument}</td>;
+                })}
+                <td>
+                  <button key={k}>View</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
