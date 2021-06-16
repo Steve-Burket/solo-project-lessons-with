f61e73e16f57c56027086c15394d2edd6f68ae00
Steve Burket <burketsteve@gmail.com>
2021-06-16 15:21:35 -0500

    updated css on student homepage

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,6 +3,10 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
 import StudentRoster from '../StudentRoster/StudentRoster.jsx';
 import PracticeLog from '../PracticeLog/PracticeLog';
+import './UserPage.css';
+
+// BOOTSTRAP!
+import Card from 'react-bootstrap//Card';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -12,10 +16,11 @@ function UserPage() {
 
   return (
     <div className='container'>
-      <h2>Hello, {user.first_name}!</h2>
-      <hr />
-      <p>Your primary instrument is: {user.instrument}</p>
-
+      <Card id='greeting'>
+        <h2>Hello, {user.first_name}!</h2>
+        <hr />
+        <p>Your primary instrument is: {user.instrument}</p>
+      </Card>
       {user.is_instructor === true && <StudentRoster />}
       {user.is_instructor === false && <PracticeLog />}
 
