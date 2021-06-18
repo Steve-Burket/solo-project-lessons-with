b8c4a9aa94e1df79e01e3201ff1437044957de87
Steve Burket <burketsteve@gmail.com>
2021-06-17 20:30:21 -0500

    Revert "updated register form"
    This reverts commit c7e105322846c3c779ab9a73c1ea73a0ffa530ae.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -16,39 +16,18 @@ function UserPage() {
   console.log('here is the students instrument:', user.instrument);
 
   function studentGreeting() {
-    if (user.is_instructor === !true) {
-      return (
-        <Card
-          body
-          id='greeting'
-          className='contact-info-container'
-          border='info'
-        >
-          <h3>Contact Info</h3>
-          <p>Your instructor is {myTeacher.first_name}</p>;
-          <p>
-            Phone #{<br />}
-            {myTeacher.phone_number}
-            {<br />}
-            Email
-            {<br />}
-            {myTeacher.email}
-          </p>
-        </Card>
-      );
-    }
+  if (user.is_instructor === !true) {
+    return <p>Your instructor is {myTeacher.first_name}</p>;
   }
-
-  // function teacherContactInfo() {}
+}
 
   return (
     <div className='container'>
       <Card id='greeting'>
         <h2>Hello, {user.first_name}!</h2>
         <hr />
+        {studentGreeting()}
       </Card>
-      {studentGreeting()}
-
       {user.is_instructor === true && <StudentRoster />}
       {user.is_instructor === false && <PracticeLog />}
 
